c85af63b578459d5284d834ade5611cd9cafc22d
victor.munoz <victor.munoz@upc.edu>
2022-04-24 11:02:53 +0200

    Creación y exportación de la función 'getAsset' para recuperar el detalle de una criptomoneda, dado su  id, usando el endpoint correspondiente de la 'API de CoinCap'.

--- src/api.js
+++ src/api.js
@@ -2,10 +2,17 @@ const url = "https://api.coincap.io/v2";
 
 function getAssets() {
   return fetch(`${url}/assets?limit=20`)
-    .then((res) => res.json())
-    .then((res) => res.data);
+    .then((respuesta) => respuesta.json())
+    .then((respuesta) => respuesta.data);
+}
+
+function getAsset(coin_id) {
+  return fetch(`${url}/assets/${coin_id}`)
+    .then((respuesta) => respuesta.json())
+    .then((respuesta) => respuesta.data);
 }
 
 export default {
   getAssets,
+  getAsset,
 };
