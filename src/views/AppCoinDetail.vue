aae2e160d754bc42659a7bf61b325b94b5fe1754
victor.munoz <victor.munoz@upc.edu>
2022-04-24 11:29:04 +0200

    Añadida la directiva Vue 'v-if' para que sólo se muestre la vista de detalle de una criptomoneda si se recibe un id correcto en la URL.

--- src/views/AppCoinDetail.vue
+++ src/views/AppCoinDetail.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-col">
-    <template>
+    <template v-if="asset.id">
       <div class="flex flex-col sm:flex-row justify-around items-center">
         <div class="flex flex-col items-center">
           <img
