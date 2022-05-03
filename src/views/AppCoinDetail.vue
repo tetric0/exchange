2c8c3f28666983ce0a14d1cfff4ea54d1cbfdd27
victor.munoz <victor.munoz@upc.edu>
2022-05-03 15:45:28 +0200

    Añadida la FUNCIONALIDAD DE CONVERSIÓN DE MONEDAS mediante COMPUTED PROPERTIES  (De BitCoin seleccionado a USD y viceversa).

--- src/views/AppCoinDetail.vue
+++ src/views/AppCoinDetail.vue
@@ -49,22 +49,27 @@
 
         <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
           <button
+            @click="toggleConverter"
             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
           >
-            Cambiar
+            {{ fromUsd ? `USD a ${asset.symbol}` : `${asset.symbol} a USD` }}
           </button>
 
           <div class="flex flex-row my-5">
             <label class="w-full" for="convertValue">
               <input
+                v-model="convertValue"
                 id="convertValue"
                 type="number"
                 class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+                :placeholder="`Valor en ${fromUsd ? 'USD' : asset.symbol}`"
               />
             </label>
           </div>
 
-          <span class="text-xl"></span>
+          <span class="text-xl"
+            >{{ convertResult }} {{ fromUsd ? `${asset.symbol}` : "USD" }}</span
+          >
         </div>
       </div>
 
@@ -124,6 +129,8 @@ export default {
       asset: {},
       history: [],
       markets: [],
+      fromUsd: true,
+      convertValue: null,
     };
   },
 
@@ -132,6 +139,18 @@ export default {
   },
 
   computed: {
+    convertResult() {
+      if (!this.convertValue) {
+        return 0;
+      }
+
+      const result = this.fromUsd
+        ? this.convertValue / this.asset.priceUsd
+        : this.convertValue * this.asset.priceUsd;
+
+      return result.toFixed(4);
+    },
+
     min() {
       return Math.min(
         // A la función que calculará el valor mínimo - Math.min() - se le tiene
@@ -161,6 +180,10 @@ export default {
   },
 
   methods: {
+    toggleConverter() {
+      this.fromUsd = !this.fromUsd;
+    },
+
     getWebsite(market) {
       this.$set(market, "isLoading", true);
 
